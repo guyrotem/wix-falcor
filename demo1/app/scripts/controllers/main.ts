379d96a93a225b1211ad21b3740d455efeee5791
David Perlman <davidp@wix.com>
2015-12-24 12:49:24 +0200

    added enter jey trigger to input

--- demo1/app/scripts/controllers/main.ts
+++ demo1/app/scripts/controllers/main.ts
@@ -17,7 +17,7 @@ class MainController {
     this.model = new falcor.Model({source: new falcor.HttpDataSource('/_api/model.json') });
   }
 
-  init() {
+  fetch() {
     // 'userSites[0..3].connectedDomains[0..2].mailboxInfo.userAccounts[0..1].userName'
     this.model.get(this.query)
       .subscribe((value: any) => {
@@ -25,6 +25,12 @@ class MainController {
           this.result = JSON.stringify(value, null , '  ');
         });
       });
+  }
+
+  watchInput(e) {
+    if (e.keyCode === 13) {
+      this.fetch();
+    }
   }
 }
 
